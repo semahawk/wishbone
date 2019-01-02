5d130369ad5780e91bbb13e17360935b1d7db6ea
Szymon Urbaś <szymon.urbas@aol.com>
2019-01-02 18:17:29 +0100

    Add the datasheet/description in a comment

--- wb_slave_register.sv
+++ wb_slave_register.sv
@@ -1,3 +1,52 @@
+/*
+ *
+ * General description:
+ *
+ *     A simple register device, which retains it's data. Reading will return
+ *     the register's data, and writing to it will modify it.
+ *
+ * Supported cycles:
+ *
+ *      SLAVE, READ / WRITE
+ *
+ * Data organization:
+ *
+ *      Port size: 8, 16, 32 or 64 bit (module parameter: DATA_WIDTH)
+ *      Port granularity: 8, 16, 32 or 64 bit (module parameter: GRANULE)
+ *      Port maximum operand size: depends on port size
+ *      Transfer ordering: Big endian
+ *      Transfer sequencing: Undefined
+ *
+ * Wishbone specification used: B4 (https://cdn.opencores.org/downloads/wbspec_b4.pdf)
+ *
+ * Copyright (c) Szymon Urbaś <szymon.urbas@aol.com> All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its
+ *        contributors may be used to endorse or promote products derived from
+ *        this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 `default_nettype none
 
 typedef enum {
